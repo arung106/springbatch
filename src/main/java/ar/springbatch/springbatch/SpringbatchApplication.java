332805d423fac4ea5d6a6dde421ed072691efd16
manjuchandramathi <manjuchandramathi@192.168.0.11>
2021-03-23 11:59:38 -0500

    commit

---
+++ src/main/java/ar/springbatch/springbatch/SpringbatchApplication.java
@@ -0,0 +1,14 @@
+package ar.springbatch.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchApplication.class, args);
+	}
+
+}
